21bf1b9526e1f0fbbf508c53e1b77407321bef98
vovitolog <vovitolog@gmail.com>
2023-05-27 12:29:12 +0300

    feat: add code splitting and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./components/Counter";
 import './index.scss'
 import MainPage from "./pages/MainPage/MainPage";
 import AboutPage from "./pages/AboutPage/AboutPage";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.asyc";
 
 const App = () => {
     return (
         <div className={'app'}>
             <Link to={'/'}>Главная </Link>
             <Link to={'/about'}>О сайте </Link>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage/>}/>
-                <Route path={'/'} element={<MainPage/>}/>
-            </Routes>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
 
     );
