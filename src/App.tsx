ea2c092b49094c18533b56d04d045eeb0df73b9c
vovitolog <vovitolog@gmail.com>
2023-05-27 12:08:04 +0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 const App = () => {
     return (
         <div className={'app'}>
-            ryryryryr
-            <Counter/>
+            <Link to={'/'}>Главная </Link>
+            <Link to={'/about'}>О сайте </Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage/>}/>
+                <Route path={'/'} element={<MainPage/>}/>
+            </Routes>
         </div>
+
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import {render} from "react-dom";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
-render (
+render(
     <div>
-        <App/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </div>, document.getElementById('root')
 )
