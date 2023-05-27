f4c4c0ba05194d562e509c61a998b5b9c628f798
vovitolog <vovitolog@gmail.com>
2023-05-27 10:41:52 +0300

    feat: add react and scss

---
+++ src/components/Counter.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import './Counter.scss'
+export const Counter = () => {
+    const [count, setCount] = useState(10);
+    const increase = () => {
+        setCount(count => count +1)
+    }
+
+    const decrease = () => {
+        setCount(count => count -1)
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increase}> Increment</button>
+            <button onClick={decrease}> Decrement</button>
+        </div>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import {render} from "react-dom";
+import {Counter} from "./components/Counter";
+
+render (
+    <div>
+        TEST REACT
+        <Counter/>
+    </div>, document.getElementById('root')
+)
