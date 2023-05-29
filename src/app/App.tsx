335e07285e14a79b89605df3cf61816927429ef2
vovitolog <vovitolog@gmail.com>
2023-05-29 20:06:18 +0300

    feat: add Navbar component and AppLink ui kit element

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {UseTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import AppRouter from "./providers/router/ui/AppRouter";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = UseTheme();
 
     return (
-        <div className={classNames('app',{}, [theme])}>
-            <button onClick={toggleTheme}>Сменить тему</button>
-            <Link to={'/'}>Главная </Link>
-            <Link to={'/about'}>О сайте </Link>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>Сменить тему</button>
         </div>
 
     );
