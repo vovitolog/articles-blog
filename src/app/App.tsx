0f44175900dffa2a68c0069f1d216069c70dff42
vovitolog <vovitolog@gmail.com>
2023-05-30 20:29:27 +0300

    feat: add i18 translation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,19 +4,22 @@ import {classNames} from "shared/lib/classNames/classNames";
 import AppRouter from "./providers/router/ui/AppRouter";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
 
 const App = () => {
     const {theme} = UseTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>
-            </div>
-        </div>
+            <Suspense fallback="">
 
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
     );
 };
 
