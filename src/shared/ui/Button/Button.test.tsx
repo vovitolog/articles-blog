5aef77cd7f5e304dc519197cba0b5e3421bf04b2
vovitolog <vovitolog@gmail.com>
2023-06-04 22:42:11 +0300

    feat: add react testing library and Button component tests

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+describe('button', () => {
+    test('render button', () => {
+        render(<Button>TEST</Button>);
+        expect(screen.getByText('TEST')).toBeInTheDocument();
+    });
+    test('render button with clear class', () => {
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
+        screen.debug();
+    });
+});
