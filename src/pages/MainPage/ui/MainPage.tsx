96c243ed479995f661c03d6b036e5dfa3ed07658
vovitolog <vovitolog@gmail.com>
2023-06-11 20:05:44 +0300

    feat: add Redux toolkit and Counter entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BugButton } from 'app/providers/ErrorBoundary';
+import { Counter } from 'enteties/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <div>
-            <BugButton />
             {t('Главная страница')}
+            <Counter />
         </div>
     );
 };
